63b4d7f32dcba2a0156313d7eae1d8ff9a063f11
Rajat Agrawal <rajat.agrawal@dbdigital.in>
2020-01-28 12:22:07 +0530

    Add DBNetworkKit file

---
+++ DBNetworkKit/DBNetworkKit.swift
@@ -0,0 +1,15 @@
+//
+//  DBNetworkKit.swift
+//  DBNetworkKit
+//
+//  Created by Rajat Agrawal on 28/01/20.
+//  Copyright © 2020 Nitin A. All rights reserved.
+//
+
+import UIKit
+
+struct DBNetworkKit {
+    enum Env {
+        case Prod, Staging
+    }
+}

15a85eb345bcb6b831f970ed0571df15b8a367f6
Rajat Agrawal <rajat.agrawal@dbdigital.in>
2020-04-02 19:29:15 +0530

    Fix bug in authentication

--- DBNetworkKit/DBAuthenticator.swift
+++ DBNetworkKit/DBAuthenticator.swift
@@ -9,149 +9,178 @@
 import UIKit
 import DBLoggingKit
 
-class DBAuthenticator {
-    static let shared = DBAuthenticator()
+public class DBAuthenticator {
+    public static let shared = DBAuthenticator()
     let queue = DispatchQueue(label: "\(DBNetworkKit.groupBundleIdentifier).authenticator")
     var beingRefreshed = false
+    let semaphore = DispatchSemaphore(value: 1)
     
-    func authenticate(completion: ((Bool)->())?) {
-        queue.async {
-            if let _ = DBNetworkKit.authToken {
-                completion?(true)
-            } else {
-                var urlComponents = DBRequestFactory.baseURLComponents()
-                urlComponents.path.append(contentsOf: DBNetworkKeys.registerAuthToken)
-                if let url = urlComponents.url {
-                    var request = DBRequestFactory.accessTokenRequest(url: url)
-                    request.httpMethod = DBNetworkManager.RequestMethod.post.rawValue
-                    
-                    let dataTask = URLSession.shared.dataTask(with: request) {
-                        data, urlResponse, error in
-                        let result = DBNetworkManager.getResponseFromData(data: data)
-                        DBLogger.shared.logMessage(message: "Result for authentication is \(result), response \(String(describing: urlResponse)), error \(String(describing: error))")
-                        if let result = result.responseData  {
-                            if let at = result[DBNetworkKit.authTokenKey] as? String {
-                                DBNetworkKit.authToken = at
-                            } else {
-                                DBLogger.shared.logMessage(message: "corrupted auth token received")
-                                completion?(false)
-                                return
-                            }
-                            
-                            if let rt = result[DBNetworkKit.refreshTokenKey] as? String {
-                                DBNetworkKit.refreshToken = rt
-                            } else {
-                                DBLogger.shared.logMessage(message: "corrupted refresh token received")
-                                completion?(false)
-                                return
-                            }
-                            
-                            if let uid = result[DBNetworkKit.uidKey] as? Int {
-                                DBNetworkKit.uid = uid
-                            } else {
-                                DBLogger.shared.logMessage(message: "corrupted uid token received")
-                                completion?(false)
-                                return
-                            }
-                            
-                            DBLogger.shared.logMessage(message: "Authentication successfull")
-                            completion?(true)
-                        } else {
-                            DBLogger.shared.logMessage(message: "corrupted data in authentication response")
-                            completion?(false)
-                        }
-                    }
-                    dataTask.resume()
-                } else {
-                    DBLogger.shared.logMessage(message: "Unable to get url for authentication")
+    public func authenticate(completion: ((Bool)->())?) {
+        semaphore.wait()
+        self.queue.async { [weak self] in
+            if let self = self {
+                DBLogger.shared.logMessage(message: "uid token during authentication is \(DBNetworkKit.uid)", level: .info, tag: "Analytics")
+                if let uid = DBNetworkKit.uid, uid != 0 {
+                    self.semaphore.signal()
                     completion?(false)
+                } else {
+                    var urlComponents = DBRequestFactory.baseURLComponents()
+                    urlComponents.path.append(contentsOf: DBNetworkKeys.registerAuthToken)
+                    if let url = urlComponents.url {
+                        var request = DBRequestFactory.accessTokenRequest(url: url)
+                        request.httpMethod = DBNetworkManager.RequestMethod.post.rawValue
+                        var success = false
+                        let dataTask = URLSession.shared.dataTask(with: request) { data, urlResponse, error in
+                            let result = DBNetworkManager.getResponseFromData(data: data)
+                            DBLogger.shared.logMessage(message: "Result for authentication is \(result), response \(String(describing: urlResponse)), error \(String(describing: error))")
+                            if let result = result.responseData  {
+                                if let at = result[DBNetworkKit.authTokenKey] as? String {
+                                    DBNetworkKit.authToken = at
+                                } else {
+                                    DBLogger.shared.logMessage(message: "corrupted auth token received", level: .info, tag: "Analytics")
+                                    self.semaphore.signal()
+                                    completion?(false)
+                                    return
+                                }
+                                
+                                if let rt = result[DBNetworkKit.refreshTokenKey] as? String {
+                                    DBNetworkKit.refreshToken = rt
+                                } else {
+                                    DBLogger.shared.logMessage(message: "corrupted refresh token received", level: .info, tag: "Analytics")
+                                    self.semaphore.signal()
+                                    completion?(false)
+                                    return
+                                }
+                                
+                                if let uid = result[DBNetworkKit.uidKey] as? Int {
+                                    DBNetworkKit.uid = uid
+                                } else {
+                                    DBLogger.shared.logMessage(message: "corrupted uid token received", level: .info, tag: "Analytics")
+                                    self.semaphore.signal()
+                                    completion?(false)
+                                    return
+                                }
+                                
+                                success = true
+                                DBLogger.shared.logMessage(message: "Authentication successfullllllll", level: .info, tag: "Analytics")
+                                self.semaphore.signal()
+                                completion?(true)
+                            } else {
+                                DBLogger.shared.logMessage(message: "corrupted data in authentication response", level: .info, tag: "Analytics")
+                                self.semaphore.signal()
+                                completion?(false)
+                            }
+                        }
+                        dataTask.resume()
+                        DBLogger.shared.logMessage(message: "Function returning \(success)", level: .info, tag: "Analytics")
+                    } else {
+                        DBLogger.shared.logMessage(message: "Unable to get url for authentication", level: .info, tag: "Analytics")
+                        self.semaphore.signal()
+                        completion?(false)
+                    }
                 }
+            } else {
+                self?.semaphore.signal()
+                completion?(false)
             }
         }
     }
     
     public func refreshAuthToken(completion: ((Bool)->())?) {
-        objc_sync_enter(self)
-        if beingRefreshed {
-            completion?(true)
-        } else {
-            beingRefreshed = true
-            queue.async {
-                var urlComponents = DBRequestFactory.baseURLComponents()
-                urlComponents.path.append(contentsOf: DBNetworkKeys.refreshAuthToken)
-                if let url = urlComponents.url {
-                    var request = DBRequestFactory.accessTokenRequest(url: url)
-                    request.httpMethod = DBNetworkManager.RequestMethod.post.rawValue
-                    let body = [ DBNetworkKit.authTokenKey : DBNetworkKit.authToken,
-                                 DBNetworkKit.refreshTokenKey : DBNetworkKit.refreshToken,
-                                 DBNetworkKit.uidKey : String(describing: DBNetworkKit.uid)
-                    ]
-                    let jsonData = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
-                    request.httpBody = jsonData
-                    
-                    let dataTask = URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
-                        if let self = self {
-                            if let _ = error {
-                                self.beingRefreshed = false
-                                completion?(false)
-                            } else {
-                                // 403
-                                if let httpUrlResponse = response as? HTTPURLResponse, httpUrlResponse.statusCode == 403 {
-                                    // save blocked user value
-                                    let _ = NSError(domain: "DBNetworkKit", code: 0, userInfo: nil)
+        queue.async { [weak self] in
+            if let self = self {
+                if self.beingRefreshed {
+                    DBLogger.shared.logMessage(message: "Being refreshed. Returning", level: .info, tag: "Analytics.Refresh")
+                    completion?(true)
+                } else {
+                    DBLogger.shared.logMessage(message: "Going to refresh", level: .info, tag: "Analytics.Refresh")
+                    self.beingRefreshed = true
+                    var urlComponents = DBRequestFactory.baseURLComponents()
+                    urlComponents.path.append(contentsOf: DBNetworkKeys.refreshAuthToken)
+                    if let url = urlComponents.url {
+                        var request = DBRequestFactory.accessTokenRequest(url: url)
+                        request.httpMethod = DBNetworkManager.RequestMethod.post.rawValue
+                        let body : [String : Any] = [ DBNetworkKit.authTokenKey : DBNetworkKit.authToken,
+                                     DBNetworkKit.refreshTokenKey : DBNetworkKit.refreshToken,
+                                     DBNetworkKit.uidKey : DBNetworkKit.uid
+                            ]
+                        let jsonData = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+                        request.httpBody = jsonData
+                        
+                        let dataTask = URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
+                            if let self = self {
+                                if let _ = error {
                                     self.beingRefreshed = false
+                                    DBLogger.shared.logMessage(message: "Unsuccessful refresh 1", level: .info, tag: "Analytics.Refresh")
                                     completion?(false)
                                 } else {
-                                    // save auth token and refresh token
-                                    if let responseJSON = DBNetworkManager.getResponseFromData(data: data).responseData  {
-                                        if let at = responseJSON[DBNetworkKit.authTokenKey] as? String,
-                                            let rt = responseJSON[DBNetworkKit.refreshTokenKey] as? String {
-                                            DBNetworkKit.authToken = at
-                                            DBNetworkKit.refreshToken = rt
-                                            self.beingRefreshed = false
-                                            completion?(true)
+                                    // 403
+                                    if let httpUrlResponse = response as? HTTPURLResponse, httpUrlResponse.statusCode == 403 {
+                                        // save blocked user value
+                                        let _ = NSError(domain: "DBNetworkKit", code: 0, userInfo: nil)
+                                        self.beingRefreshed = false
+                                        DBLogger.shared.logMessage(message: "Unsuccessful refresh 2", level: .info, tag: "Analytics.Refresh")
+                                        completion?(false)
+                                    } else {
+                                        // save auth token and refresh token
+                                        if let responseJSON = DBNetworkManager.getResponseFromData(data: data).responseData  {
+                                            if let at = responseJSON[DBNetworkKit.authTokenKey] as? String,
+                                                let rt = responseJSON[DBNetworkKit.refreshTokenKey] as? String {
+                                                DBNetworkKit.authToken = at
+                                                DBNetworkKit.refreshToken = rt
+                                                self.beingRefreshed = false
+                                                DBLogger.shared.logMessage(message: "Successful refresh", level: .info, tag: "Analytics.Refresh")
+                                                completion?(true)
+                                            } else {
+                                                let _ = NSError(domain: "DBNetworkKit", code: 0, userInfo: nil)
+                                                self.beingRefreshed = false
+                                                DBLogger.shared.logMessage(message: "Unsuccessful refresh 3", level: .info, tag: "Analytics.Refresh")
+                                                completion?(false)
+                                            }
                                         } else {
                                             let _ = NSError(domain: "DBNetworkKit", code: 0, userInfo: nil)
                                             self.beingRefreshed = false
+                                            DBLogger.shared.logMessage(message: "Unsuccessful refresh 4", level: .info, tag: "Analytics.Refresh")
                                             completion?(false)
                                         }
-                                    } else {
-                                        let _ = NSError(domain: "DBNetworkKit", code: 0, userInfo: nil)
-                                        self.beingRefreshed = false
-                                        completion?(false)
                                     }
                                 }
+                            } else {
+                                self?.beingRefreshed = false
+                                DBLogger.shared.logMessage(message: "Unsuccessful refresh 5", level: .info, tag: "Analytics.Refresh")
+                                completion?(false)
                             }
-                        } else {
-                            self?.beingRefreshed = false
-                            completion?(false)
                         }
+                        dataTask.resume()
+                    } else {
+                        // Add error here later
+                        let _ = NSError(domain: "DBNetworkKit", code: 0, userInfo: nil)
+                        self.beingRefreshed = false
+                        DBLogger.shared.logMessage(message: "Unsuccessful refresh 6", level: .info, tag: "Analytics.Refresh")
+                        completion?(false)
                     }
-                    dataTask.resume()
-                } else {
-                    // Add error here later
-                    let _ = NSError(domain: "DBNetworkKit", code: 0, userInfo: nil)
-                    self.beingRefreshed = false
-                    completion?(false)
                 }
+            } else {
+                // self not present
+                DBLogger.shared.logMessage(message: "Unsuccessful refresh 7", level: .info, tag: "Analytics.Refresh")
+                completion?(false)
             }
         }
-        objc_sync_exit(self)
     }
     
-    func authToken(completion: ((String?)->())?) {
+    public func authToken(completion: ((String?)->())?) {
         queue.async {
             completion?(DBNetworkKit.authToken)
         }
     }
     
-    func refreshToken(completion: ((String?)->())?) {
+    public func refreshTokenValue(completion: ((String?)->())?) {
         queue.async {
             completion?(DBNetworkKit.refreshToken)
         }
     }
     
-    func uid(completion: ((Int?)->())?) {
+    public func uid(completion: ((Int?)->())?) {
         queue.async {
             completion?(DBNetworkKit.uid)
         }
